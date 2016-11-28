151b960dbe3f3a3a053cbf656d1f7cbcc3aec1ec
Steven Fackler <sfackler@gmail.com>
2016-11-27 22:05:45 -0800

    Server side support

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ extern crate hyper;
 extern crate openssl;
 
 use antidote::Mutex;
-use hyper::net::{SslClient, NetworkStream};
+use hyper::net::{SslClient, SslServer, NetworkStream};
 use openssl::error::ErrorStack;
-use openssl::ssl::{self, SslMethod, SslConnector, SslConnectorBuilder};
+use openssl::ssl::{self, SslMethod, SslConnector, SslConnectorBuilder, SslAcceptor};
 use std::io::{self, Read, Write};
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -34,6 +34,28 @@ impl<T> SslClient<T> for OpensslClient
 
     fn wrap_client(&self, stream: T, host: &str) -> hyper::Result<SslStream<T>> {
         match self.0.connect(host, stream) {
+            Ok(stream) => Ok(SslStream(Arc::new(Mutex::new(stream)))),
+            Err(err) => Err(hyper::Error::Ssl(Box::new(err))),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct OpensslServer(Arc<SslAcceptor>);
+
+impl From<SslAcceptor> for OpensslServer {
+    fn from(acceptor: SslAcceptor) -> OpensslServer {
+        OpensslServer(Arc::new(acceptor))
+    }
+}
+
+impl<T> SslServer<T> for OpensslServer
+    where T: NetworkStream + Clone + Sync + Send + Debug
+{
+    type Stream = SslStream<T>;
+
+    fn wrap_server(&self, stream: T) -> hyper::Result<SslStream<T>> {
+        match self.0.accept(stream) {
             Ok(stream) => Ok(SslStream(Arc::new(Mutex::new(stream)))),
             Err(err) => Err(hyper::Error::Ssl(Box::new(err))),
         }
@@ -75,11 +97,16 @@ impl<T: NetworkStream> NetworkStream for SslStream<T> {
 
 #[cfg(test)]
 mod test {
-    use hyper::Client;
+    use hyper::{Client, Server};
+    use hyper::server::{Request, Response, Fresh};
     use hyper::net::HttpsConnector;
+    use openssl::ssl::{SslMethod, SslAcceptorBuilder, SslConnectorBuilder};
+    use openssl::pkey::PKey;
+    use openssl::x509::X509;
     use std::io::Read;
+    use std::mem;
 
-    use OpensslClient;
+    use {OpensslClient, OpensslServer};
 
     #[test]
     fn google() {
@@ -91,5 +118,42 @@ mod test {
         assert!(resp.status.is_success());
         let mut body = vec![];
         resp.read_to_end(&mut body).unwrap();
+    }
+
+    #[test]
+    fn server() {
+        let cert = include_bytes!("../test/cert.pem");
+        let key = include_bytes!("../test/key.pem");
+
+        let cert = X509::from_pem(cert).unwrap();
+        let key = PKey::private_key_from_pem(key).unwrap();
+
+        let acceptor = SslAcceptorBuilder::mozilla_intermediate(SslMethod::tls(),
+                                                                &key,
+                                                                &cert,
+                                                                None::<X509>)
+            .unwrap()
+            .build();
+        let ssl = OpensslServer::from(acceptor);
+        let server = Server::https("0.0.0.0:0", ssl).unwrap();
+
+        let listening = server.handle(|_: Request, resp: Response<Fresh>| {
+            resp.send(b"hello").unwrap()
+        }).unwrap();
+        let port = listening.socket.port();
+        mem::forget(listening);
+
+        let mut connector = SslConnectorBuilder::new(SslMethod::tls()).unwrap();
+        connector.builder_mut().set_ca_file("test/cert.pem").unwrap();
+        let ssl = OpensslClient::from(connector.build());
+        let connector = HttpsConnector::new(ssl);
+        let client = Client::with_connector(connector);
+
+        let mut resp = client.get(&format!("https://localhost:{}", port))
+            .send()
+            .unwrap();
+        let mut body = vec![];
+        resp.read_to_end(&mut body).unwrap();
+        assert_eq!(body, b"hello");
     }
 }
