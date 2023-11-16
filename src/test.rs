eb6b7d1e5d72badf2cbf0aa363c9a83b6c92ea81
Steven Fackler <sfackler@gmail.com>
2023-11-15 21:15:03 -0500

    test client and server

--- src/test.rs
+++ src/test.rs
@@ -1,23 +1,52 @@
-use std::pin::Pin;
+use std::{io, pin::Pin};
 
-use hyper::{client::conn::http1, header::HOST, Request};
+use hyper::{client::conn::http1, header::HOST, server, service, Request, Response};
 use hyper_util::rt::TokioIo;
-use openssl::ssl::{SslConnector, SslMethod};
-use tokio::net::TcpStream;
+use openssl::ssl::{Ssl, SslAcceptor, SslConnector, SslFiletype, SslMethod};
+use tokio::net::{TcpListener, TcpStream};
 
 use crate::SslStream;
 
 #[tokio::test]
-async fn google() {
-    let stream = TcpStream::connect("google.com:443").await.unwrap();
+async fn client_server() {
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    tokio::spawn(async move {
+        let mut acceptor = SslAcceptor::mozilla_intermediate_v5(SslMethod::tls()).unwrap();
+        acceptor
+            .set_private_key_file("test/key.pem", SslFiletype::PEM)
+            .unwrap();
+        acceptor
+            .set_certificate_chain_file("test/cert.pem")
+            .unwrap();
+        let acceptor = acceptor.build();
+
+        let ssl = Ssl::new(acceptor.context()).unwrap();
+        let stream = listener.accept().await.unwrap().0;
+        let mut stream = SslStream::new(ssl, TokioIo::new(stream)).unwrap();
+
+        Pin::new(&mut stream).accept().await.unwrap();
+
+        let service =
+            service::service_fn(|_| async { Ok::<_, io::Error>(Response::new(String::new())) });
+
+        server::conn::http1::Builder::new()
+            .serve_connection(stream, service)
+            .await
+            .unwrap();
+    });
+
+    let stream = TcpStream::connect(addr).await.unwrap();
     let stream = TokioIo::new(stream);
 
-    let ssl = SslConnector::builder(SslMethod::tls())
-        .unwrap()
+    let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    builder.set_ca_file("test/cert.pem").unwrap();
+    let ssl = builder
         .build()
         .configure()
         .unwrap()
-        .into_ssl("google.com")
+        .into_ssl("localhost")
         .unwrap();
     let mut stream = SslStream::new(ssl, stream).unwrap();
     Pin::new(&mut stream).connect().await.unwrap();
